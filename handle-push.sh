1d9c7d231f204a29005447a87d2aaddd1d685366
Roman Galeev <roman.galeev@ezuce.com>
2017-10-13 12:21:35 +0200

    Rebuild reach-ui-jh on commits to jamhed-devel branch

--- handle-push.sh
+++ handle-push.sh
@@ -9,6 +9,9 @@ unset NODE
 if [ $REPO = "reach3" ] && [ $BRANCH = "refs/heads/master" ]
 then
 	docker exec reach.ezuce bash -c 'cd reach && git fetch && git reset --hard origin/master'
+elif [ $REPO = "reach-ui" && [ $BRANCH = "refs/heads/jamhed-devel" ]
+then
+	cd ~/docker/reach-ui-jh && ./build.sh && ./run.sh
 else
 	echo skip $REPO $BRANCH
 fi
