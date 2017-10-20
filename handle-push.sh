0ba0baa263fdfefd12d0d7c3832c9308d654ecfa
Roman Galeev <roman.galeev@ezuce.com>
2017-10-20 02:42:35 +0200

    Fix missing bracked

--- handle-push.sh
+++ handle-push.sh
@@ -9,7 +9,7 @@ unset NODE
 if [ $REPO = "reach3" ] && [ $BRANCH = "refs/heads/master" ]
 then
 	docker exec reach.ezuce bash -c 'cd reach && git fetch && git reset --hard origin/master'
-elif [ $REPO = "reach-ui" && [ $BRANCH = "refs/heads/jamhed-devel" ]
+elif [ $REPO = "reach-ui" ] && [ $BRANCH = "refs/heads/jamhed-devel" ]
 then
 	cd ~/docker/reach-ui-jh && ./build.sh && ./run.sh
 else
