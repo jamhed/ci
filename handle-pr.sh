cfca01614d7d73f09e9065c6248ac3c81f98414a
Roman Galeev <roman.galeev@ezuce.com>
2017-11-24 10:51:27 +0100

    Must use commit/branch as a deployment reference

--- handle-pr.sh
+++ handle-pr.sh
@@ -15,7 +15,7 @@ unset NODE
 function create_deployment() {
 	curl -s -X POST \
 		-H "Content-Type: application/json" \
-		-d "{ \"ref\": \"$REPO-$PR\", \"payload\": \"Docker Deploy\", \"auto_merge\": false }" \
+		-d "{ \"ref\": \"$COMMIT\", \"payload\": \"Docker Deploy\", \"auto_merge\": false }" \
 		"https://api.github.com/repos/$REPO/deployments?access_token=$TOKEN" | jq '.id'
 }
 
